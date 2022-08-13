6ee65047aee9fb113bddeab3e2a5b57c124b23bf
Valeentin87 <Valeentin87@list.ru>
2022-08-13 18:34:40 +0300

    Избавился от проблем с null

--- Zadacha10/Program.cs
+++ Zadacha10/Program.cs
@@ -1,6 +1,7 @@
 ﻿/* Задача 10. Напишите программу, которая принимает на вход трёхзначное чсило и на выходе показывает вторую цифру этого числа*/
 Console.WriteLine("Введите трёх значное число");
-char Vtoroe = SecondNumber(Console.ReadLine());
+string? A = Console.ReadLine();
+char Vtoroe = SecondNumber(A);
 Console.WriteLine($"Второе число равно = {Vtoroe}");
 char SecondNumber(string number)
 {
