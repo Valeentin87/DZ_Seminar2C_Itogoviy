28eea44c278a9019c785ec51ff9733aea2a305ae
Valeentin87 <Valeentin87@list.ru>
2022-08-13 18:27:15 +0300

    поменяли текст программы к задаче 13 с учётом замечаний

--- Zadacha13/Program.cs
+++ Zadacha13/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: напишите программу которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+Console.WriteLine("Введите целое число, не менее трёх знаков");
+string A = Console.ReadLine();
+string Tretia = numberThRee (A);
+Console.WriteLine($"третья цифра во введенном числе равна {Tretia}");
+string numberThRee (string Nstring)
+{
+        if (Nstring.Length < 3) 
+    {
+        string name = "Во введенном числе третьей цифры нет";
+        return name;
+    }
+    else
+    {
+        return Convert.ToString(Nstring[2]);
+    }
+} 
