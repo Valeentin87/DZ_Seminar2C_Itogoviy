cbf437c92407e796a890698429af5e10dd9a5455
Valeentin87 <Valeentin87@list.ru>
2022-08-13 18:40:23 +0300

    написал новый текст программы с учётом замечаний

--- Zadacha15/Program.cs
+++ Zadacha15/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает цифру, обозначающую день недели и проверяет не является ли этот день выходным
+Console.WriteLine("Введите цифру, обозначающую порядковый день в неделе");
+int days = Convert.ToInt32(Console.ReadLine());
+Console.Write(Vihodnoy(days));
+string Vihodnoy (int day)
+{
+    if (day == 6 || day == 7)
+    {
+        string otvet = "день является выходным";
+        return otvet;
+    }
+    else
+    {
+        string otvet = "день является рабочим";
+        return otvet;
+    }
+    
+}
